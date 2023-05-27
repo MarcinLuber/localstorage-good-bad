bc450acc1b4c5237fe8e0c734523725404286913
MarcinLuber <72253623+MarcinLuber@users.noreply.github.com>
2023-05-27 19:13:36 +0200

    Add files via upload

--- aps.js
+++ aps.js
@@ -1,18 +1,21 @@
+
 const btn1 = document.querySelector('.good');
 const btn2 = document.querySelector('.bad');
 const btn3 = document.querySelector('.reset-g');
 const h2g = document.querySelector('.n-good');
 const h2b = document.querySelector('.n-bad');
 
-let counter = localStorage.getItem('counter');
-if (counter === null) {
-  counter = {
-    count1: 0,
-    count2: 0,
-  };
-} else {
-  counter = JSON.parse(counter);
+let counter = localStorage.getItem('counter')
+if(counter === null){
+counter = {
+ count1:0,
+ count2:0,};
+ }else {
+counter = JSON.parse(counter);
 }
+
+
+
 
 // let count1 = Number(localStorage.getItem('count1'));
 // let count2 = Number(localStorage.getItem('count2'));
@@ -20,14 +23,16 @@ h2g.textContent = counter.count1;
 h2b.textContent = counter.count2;
 
 btn1.addEventListener('click', () => {
-  h2g.textContent = ++counter.count1;
-  localStorage.setItem('counter', JSON.stringify(counter));
-});
-btn2.addEventListener('click', () => {
-  // eslint-disable-next-line no-plusplus
-  h2b.textContent = ++counter.count2;
-  localStorage.setItem('counter', JSON.stringify(counter));
-});
-btn3.addEventListener('click', () => {
-});
-localStorage.removeItem('counter');
+    h2g.textContent = ++counter.count1
+    localStorage.setItem('counter',JSON.stringify(counter));
+   });
+   btn2.addEventListener('click', () => {
+    h2b.textContent = ++counter.count2;
+    localStorage.setItem('counter', JSON.stringify(counter));
+   });
+   btn3.addEventListener('click', ()=> {
+   localStorage.removeItem('counter')
+  
+
+   });
+   
